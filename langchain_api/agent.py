aea1ffbbb4912cc7f534b41d74cea9a61f492f2c
A800 <jiawena.yang@archermind.com>
2025-12-28 20:30:26 +0800

    初次提交

---
+++ langchain_api/agent.py
@@ -0,0 +1,50 @@
+from langchain.agents import create_agent
+from langchain.agents.middleware import (
+    HumanInTheLoopMiddleware,
+)
+from langchain_openai import ChatOpenAI
+from langchain_api.middleware import PlanningMiddleware
+from langgraph.checkpoint.memory import MemorySaver
+
+checkpointer = MemorySaver()
+
+
+def add(a: float, b: float) -> float:
+    """Add two numbers."""
+    return a + b
+
+
+def multiply(a: float, b: float) -> float:
+    """Multiply two numbers."""
+    return a * b
+
+
+class Agent:
+    def __init__(self):
+        self.model = ChatOpenAI(
+            model="qwen3",
+            base_url="http://localhost:8082/v1",
+            api_key="your_api_key",
+            tags=["agent"],
+        )
+
+        tools = [add, multiply]
+        self.agent = create_agent(
+            model=self.model,
+            tools=tools,
+            system_prompt="你是一个善于使用工具的助手。 你每次只能使用一个工具，禁止一次调用多个工具。",
+            middleware=[
+                PlanningMiddleware(
+                    model=ChatOpenAI(
+                        model="qwen3",
+                        base_url="http://localhost:8082/v1",
+                        api_key="your_api_key",
+                    )
+                ),
+                HumanInTheLoopMiddleware(interrupt_on={"add": True}),
+            ],
+            checkpointer=checkpointer,
+        )
+
+    def get_agent(self):
+        return self.agent
